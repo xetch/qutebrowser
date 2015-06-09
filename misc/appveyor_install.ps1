022856796ff09fe0a800f47ea6fdcc5622d63fc8
Florian Bruhin <git@the-compiler.org>
2015-06-09 19:59:06 +0200

    copy python

--- misc/appveyor_install.ps1
+++ misc/appveyor_install.ps1
@@ -4,14 +4,19 @@
 # Adapted by Mark Harviston <mark.harviston@gmail.com>
 # Adapted again by Florian Bruhin <mail@qutebrowser.org>.
 
-$BASE_URL = "https://www.python.org/ftp/python/"
-
 function InstallPackage ($python_home, $pkg) {
     $pip_path = $python_home + "\Scripts\pip.exe"
     & $pip_path install $pkg
 }
 
+function CopyPython ($python_home) {
+    $old_path = $python_home + "\python.exe"
+    $new_path = $python_home + "\python3.exe"
+    & Copy-Item $old_path $new_path
+}
+
 function main () {
+    CopyPython $env:PYTHON
     InstallPackage $env:PYTHON tox
     (new-object net.webclient).DownloadFile("http://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.4.1/PyQt5-5.4.1-gpl-Py3.4-Qt5.4.1-x64.exe", "C:\install-PyQt5.exe")
     Start-Process -FilePath C:\install-PyQt5.exe -ArgumentList "/S" -Wait -Passthru
